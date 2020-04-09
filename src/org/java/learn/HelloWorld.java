1954b3a7f07bbbb27b709b4856301b0593c172e4
Dell <Dell@Dell-PC>
2020-04-09 22:45:01 +0530

    First Commit

---
+++ src/org/java/learn/HelloWorld.java
@@ -0,0 +1,19 @@
+package org.java.learn;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// single line comments
+		
+		System.out.println("Hello World!");
+		
+		
+		/*
+		 * this is the multiple line comments
+		 */	
+
+	
+	}
+
+}
